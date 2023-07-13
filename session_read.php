732f8e94afea2f6459caffacf636ee2f545a9d09
kyaorin <kyaorin0930@gmail.com>
2023-07-13 23:18:00 +0900

    セッション履歴作成

--- session_read.php
+++ session_read.php
@@ -3,6 +3,33 @@ session_start();
 include('functions.php');
 check_session_id();
 
+$pdo = connect_to_db();
+
+$sql = 'SELECT * FROM session_record_table ORDER BY session_date DESC';
+$stmt = $pdo->prepare($sql);
+
+try {
+    $status = $stmt->execute();
+} catch (PDOException $e) {
+    echo json_encode(["sql error" => "{$e->getMessage()}"]);
+    exit();
+}
+
+
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$output = "";
+foreach ($result as $record) {
+    $output .= "
+    <fieldset>
+    <tr>
+      <td>{$record["session_date"]}</td><br><br>
+      <td>サポーター：{$record["supporter"]}</td><br><br>
+      <td>相談内容：{$record["session_content"]}</td><br><br>
+      <td>振返メモ：{$record["session_memo"]}</td>
+    </tr>
+    </fieldset><br>
+  ";
+}
 
 ?>
 
@@ -17,6 +44,9 @@ check_session_id();
 
 <body>
     <h2><?= $_SESSION['u_name'] ?> さんのセッション履歴一覧</h2>
+    <tbody>
+        <?= $output ?>
+    </tbody>
     <div class=session_list>
         <a href="session_input.php">セッション履歴入力画面へ</a>
     </div>
